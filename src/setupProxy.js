9b05e9ec2bcdec49a58de89e9866bdc6572cd916
kimdeagle <juhyeok.dev@gmail.com>
2023-06-29 16:40:24 +0900

    dotenv 내 변수명 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     process.env.REACT_APP_BASE_URL,
     createProxyMiddleware({
-      target: process.env.REACT_APP_API_URL,
+      target: process.env.REACT_APP_API_DOMAIN,
       changeOrigin: true,
     })
   );
